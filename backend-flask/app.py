94380f5f2a12685a6f354681aa24ffe89ca0dcb5
Raj Mourya <rajhmourya@gmail.com>
2023-03-12 02:49:11 +0530

    added segmentation endpoint

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -28,50 +28,109 @@ s3 = boto3.client('s3',
 
 BUCKET_NAME='codevibecressendo2k23'
 
+
+from random import randint
+
+def random_with_N_digits(n):
+    range_start = 10**(n-1)
+    range_end = (10**n)-1
+    return randint(range_start, range_end)
+
 # @app.route('/')  
 # def home():
 #     return render_template("file_upload_to_s3.html")
-# @app.route('/segment',methods=['post'])
-# def segment():
-#     if request.method == 'POST':
-#         file = request.files['file']
-#         img = cv2.imdecode(np.frombuffer(file.read(), np.uint8), cv2.IMREAD_UNCHANGED)
+from matplotlib import pyplot as plt
+# from skimage.morphology import extrema
+# from skimage.morphology import watershed as skwater
+
+def ShowImage(title,img,ctype):
+    plt.figure(figsize=(10, 10))
+    if ctype=='bgr':
+        b,g,r = cv2.split(img)       # get b,g,r
+        rgb_img = cv2.merge([r,g,b])     # switch it to rgb
+        plt.imshow(rgb_img)
+    elif ctype=='hsv':
+        rgb = cv2.cvtColor(img,cv2.COLOR_HSV2RGB)
+        plt.imshow(rgb)
+    elif ctype=='gray':
+        plt.imshow(img,cmap='gray')
+    elif ctype=='rgb':
+        plt.imshow(img)
+    else:
+        raise Exception("Unknown colour type")
+    plt.axis('off')
+    plt.title(title)
+    plt.show()
+
+
+from io import BytesIO
+
+
+@app.route('/segment',methods=['post'])
+def segment():
+    if request.method == 'POST':
+        file = request.files['file']
+        fname =  'codeVibe'+str(random_with_N_digits(4))+'.jpg'
+        file.save(fname)
+        img = cv2.imread(fname)
+        gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+        # ShowImage('Brain MRI',gray,'gray')
+
+        ret, thresh = cv2.threshold(gray,0,255,cv2.THRESH_OTSU)
+        # ShowImage('Thresholding image',thresh,'gray')
+
+        ret, markers = cv2.connectedComponents(thresh)
+
+        #Get the area taken by each component. Ignore label 0 since this is the background.
+        marker_area = [np.sum(markers==m) for m in range(np.max(markers)) if m!=0] 
+        #Get label of largest component by area
+        largest_component = np.argmax(marker_area)+1 #Add 1 since we dropped zero above                        
+        #Get pixels which correspond to the brain
+        brain_mask = markers==largest_component
+
+        brain_out = img.copy()
+        #In a copy of the original image, clear those pixels that don't correspond to the brain
+        brain_out[brain_mask==False] = (0,0,0)
+
+        img = cv2.imread(fname)
+        gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+        ret, thresh = cv2.threshold(gray,0,255,cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
+
+
+        # noise removal
+        kernel = np.ones((3,3),np.uint8)
+        opening = cv2.morphologyEx(thresh,cv2.MORPH_OPEN,kernel, iterations = 2)
+
+        # sure background area
+        sure_bg = cv2.dilate(opening,kernel,iterations=3)
+
+        # Finding sure foreground area
+        dist_transform = cv2.distanceTransform(opening,cv2.DIST_L2,5)
+        ret, sure_fg = cv2.threshold(dist_transform,0.7*dist_transform.max(),255,0)
+
+        # Finding unknown region
+        sure_fg = np.uint8(sure_fg)
+        unknown = cv2.subtract(sure_bg,sure_fg)
+
+        # Marker labelling
+        ret, markers = cv2.connectedComponents(sure_fg)
+
+        # Add one to all labels so that sure background is not 0, but 1
+        markers = markers+1
+
+
+        # Now, mark the region of unknown with zero
+        markers[unknown==255] = 0
+        markers = cv2.watershed(img,markers)
+        img[markers == -1] = [255,0,0]
+
+        im1 = cv2.cvtColor(img,cv2.COLOR_HSV2RGB)
+        image_bytes = cv2.imencode('.jpg', im1)[1].tobytes()
+        i = BytesIO(image_bytes)
+        cv2.imwrite(fname, im1)
+        return upload(i,fname)
+        # ShowImage('Watershed segmented image',im1,'gray')
         
-#         # Convert the image to grayscale
-#         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        
-#         # Apply thresholding to segment the image
-#         ret, thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
-        
-#         # Apply morphological operations to remove noise
-#         kernel = np.ones((3,3), np.uint8)
-#         opening = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel, iterations=2)
-#         sure_bg = cv2.dilate(opening, kernel, iterations=3)
-        
-#         # Apply distance transform to find the foreground objects
-#         dist_transform = cv2.distanceTransform(opening, cv2.DIST_L2, 5)
-#         ret, sure_fg = cv2.threshold(dist_transform, 0.7*dist_transform.max(), 255, 0)
-        
-#         # Find the unknown region
-#         sure_fg = np.uint8(sure_fg)
-#         unknown = cv2.subtract(sure_bg, sure_fg)
-        
-#         # Apply the watershed algorithm to segment the image
-#         ret, markers = cv2.connectedComponents(sure_fg)
-#         markers = markers + 1
-#         markers[unknown==255] = 0
-#         markers = cv2.watershed(img, markers)
-        
-#         # Create a color map for the segmented image
-#         colormap = np.zeros((markers.shape[0], markers.shape[1], 3), dtype=np.uint8)
-#         colormap[markers == -1] = [0, 0, 255] # Boundary
-#         colormap[markers == 1] = [0, 255, 0] # Background
-#         colormap[markers > 1] = [255, 0, 0] # Foreground
-        
-#         # Convert the color map to a JPEG image
-#         ret, jpeg = cv2.imencode('.jpg', colormap)
-#         print(jpeg)
-#         return upload(jpeg,"demo.jpg")
 
 
 # @app.route('/upload',methods=['post'])
@@ -80,7 +139,7 @@ def upload(img,filename):
     #     img = request.files['file']
     if img:
             filename = secure_filename(filename)
-            img.save(filename)
+            # img.save(filename)
             s3.upload_file(
                 Bucket = BUCKET_NAME,
                 Filename=filename,
