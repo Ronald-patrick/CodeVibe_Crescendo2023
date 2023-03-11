580a28dbec0cc0e71f6e1f1b2c6081e3f66d34ca
iammeet071 <meetparmar071@gmail.com>
2023-03-11 20:13:07 +0530

    fixed error

--- frontend - web/src/App.jsx
+++ frontend - web/src/App.jsx
@@ -1,14 +1,13 @@
 import { Routes, Route } from "react-router-dom";
-import Landing from "./pages/Landing";
 import Login from "./pages/Auth/Login";
 import Register from "./pages/Auth/Register";
-import AllPatients from "./pages/AllPatients";
+
 import RegisterHP from "./pages/RegisterHP";
 import ReportList from "./components/ReportList";
 import PatientRegister from './pages/PatientRegister';
 import SearchPatientList from './pages/SearchPatientList';
 import SendRequest from './pages/SendRequest';
-import Dashboard from './pages/Dashboard';
+
 import ViewPatient from "./components/ViewPatient";
 
 function App() {
@@ -17,13 +16,13 @@ function App() {
       <Route path='/' element={<ViewPatient />} />
       <Route path='/login' element={<Login />} />
       <Route path='/register' element={<Register />} />
-      <Route path='/dashboard' element={<AllPatients />} />
+      
       <Route path='/registerhp' element={<RegisterHP />} />
       <Route path='/reportlist' element={<ReportList />} />
     <Route path='/patientRegister' element={<PatientRegister />} />
     <Route path = "/listPatients" element={<SearchPatientList/>} />
     <Route path = "/sendReq" element={<SendRequest/>} />
-    <Route path = "/Dashboard" element={<Dashboard/>} />
+
   
     
 
