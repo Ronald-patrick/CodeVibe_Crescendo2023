6cef8841bb74a4e9ca56e0cc15de0e090eb80681
Raj Mourya <rajhmourya@gmail.com>
2023-03-11 23:00:34 +0530

    added reuested fetching from backend

--- frontend - app/src/components/Util/RequestsScreen.js
+++ frontend - app/src/components/Util/RequestsScreen.js
@@ -1,25 +1,24 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import React,{useState,useEffect} from 'react';
+import React, {useState, useEffect} from 'react';
 import {FlatList, StyleSheet, Text, View} from 'react-native';
 import RequestsItem from './RequestsItem';
 import axios from 'axios';
 const data = [
-  {id: '1', PH1Name: 'Item 1',PHPhone: 'Phone 1',PH2Name: 'Item 2'},
-  {id: '2', PH1Name: 'Item 1',PHPhone: 'Phone 1',PH2Name: 'Item 2'},
-  {id: '3', PH1Name: 'Item 1',PHPhone: 'Phone 1',PH2Name: 'Item 2'},
-  {id: '4', PH1Name: 'Item 1',PHPhone: 'Phone 1',PH2Name: 'Item 2'},
-  {id: '5', PH1Name: 'Item 1',PHPhone: 'Phone 1',PH2Name: 'Item 2'},
+  {id: '1', PH1Name: 'Item 1', PHPhone: 'Phone 1', PH2Name: 'Item 2'},
+  {id: '2', PH1Name: 'Item 1', PHPhone: 'Phone 1', PH2Name: 'Item 2'},
+  {id: '3', PH1Name: 'Item 1', PHPhone: 'Phone 1', PH2Name: 'Item 2'},
+  {id: '4', PH1Name: 'Item 1', PHPhone: 'Phone 1', PH2Name: 'Item 2'},
+  {id: '5', PH1Name: 'Item 1', PHPhone: 'Phone 1', PH2Name: 'Item 2'},
 ];
 
 const renderItem = ({item}) => (
-//   <View style={styles.item}>
-//     <Text>{item.title}</Text>
-//   </View>
-<View>
-    <RequestsItem item = {item}/>
-</View>
+  //   <View style={styles.item}>
+  //     <Text>{item.title}</Text>
+  //   </View>
+  <View>
+    <RequestsItem item={item} />
+  </View>
 );
-
 
 _retrieveData = async () => {
   try {
@@ -36,9 +35,7 @@ _retrieveData = async () => {
 };
 
 export default function RequestsScreen() {
-
   const [data, setData] = useState([]);
-
 
   const demo = async () => {
     let tokenGot = await _retrieveData();
@@ -53,11 +50,18 @@ export default function RequestsScreen() {
       {},
       config,
     );
-    console.log("requests apge: ",res.data)
-    // setData(ls);
+
+    console.log('requests apge: ', res.data.requests);
+    let ls = res.data.requests;
+    let d = [];
+    for (let i = 0; i < ls.length; i++) {
+      d.push({id:i+1, name: ls[i].name,address: ls[i].address,phone_number: ls[i].phone_number});
+    }
+    console.log('d: ', d);
+    setData(ls);
   };
   useEffect(() => {
-   demo();
+    demo();
   }, []);
   return (
     <View style={styles.container}>
