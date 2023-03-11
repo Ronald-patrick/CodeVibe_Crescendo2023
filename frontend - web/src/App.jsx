c7cd5936c80395ed7bdb17317287cb1f3f5b752b
iammeet071 <meetparmar071@gmail.com>
2023-03-11 21:45:57 +0530

    added patientdetail

--- frontend - web/src/App.jsx
+++ frontend - web/src/App.jsx
@@ -4,32 +4,27 @@ import Register from "./pages/Auth/Register";
 
 import RegisterHP from "./pages/RegisterHP";
 import ReportList from "./components/ReportList";
-import PatientRegister from './pages/PatientRegister';
-import SearchPatientList from './pages/SearchPatientList';
-import SendRequest from './pages/SendRequest';
-import Dashboard from './pages/Dashboard';
-
+import PatientRegister from "./pages/PatientRegister";
+import SearchPatientList from "./pages/SearchPatientList";
+import SendRequest from "./pages/SendRequest";
+import Dashboard from "./pages/Dashboard";
+import Landing from "./pages/Landing";
+import PatientDetailsCard from "./pages/PatientDetailsCard";
 function App() {
   return (
     <Routes>
-      <Route path="/Dashboard" element={<Dashboard />} />
+      <Route path='/Dashboard' element={<Dashboard />} />
       <Route path='patientRegister' element={<PatientRegister />} />
-      <Route path="listPatients" element={<SearchPatientList />} />
-      <Route index path='/' element={<ViewPatient />} />
+      <Route path='listPatients' element={<SearchPatientList />} />
+      <Route index path='/' element={<Landing />} />
       <Route path='/login' element={<Login />} />
       <Route path='/register' element={<Register />} />
-
+      <Route path='/patientcard' element={<PatientDetailsCard />} />
       <Route path='/registerhp' element={<RegisterHP />} />
       <Route path='/reportlist' element={<ReportList />} />
-      <Route path="/sendReq" element={<SendRequest />} />
-
-
-
-
+      <Route path='/sendReq' element={<SendRequest />} />
     </Routes>
   );
-
-
 }
 
 export default App;
--- frontend - web/src/pages/RegisterHP.jsx
+++ frontend - web/src/pages/RegisterHP.jsx
@@ -1,47 +1,28 @@
-import React from "react";
-
-import { useFormik } from "formik";
-import * as yup from "yup";
-
 import { Button, TextField } from "@mui/material";
-
-
-const validationSchema = yup.object({
-  hp_id: yup.string("Enter your hp_id").required("hp_id is required"),
-  name: yup.string("Enter your name").required("name is required"),
-  address: yup.string("Enter your address").required("address is required"),
-  phoneNo: yup
-    .string("Enter your Phone No")
-    .matches(/^[6-9]\d{9}$/, {
-      message: "Please enter valid number.",
-      excludeEmptyString: false,
-    })
-    .required("Phone No required")
-    .min(10, "too short")
-    .max(10, "too long"),
-});
-const RegisterHP = () => {
-  const formik = useFormik({
-    initialValues: {
-      hp_id: "",
-      name: "",
-      address: "",
-      phoneNo: "",
-    },
-    validationSchema: validationSchema,
-  });
+import NavBasic from "../components/NavBasic"
+export default function RegisterHP() {
   return (
     <>
+    <NavBasic/>
+    
+      {/* <div className="hidden sm:block" aria-hidden="true">
+                  <div className="py-5">
+                      <div className="border-t border-gray-200" />
+                  </div>
+              </div> */}
+
       <div className='mt-10 sm:mt-0'>
-        <div className='md:grid md:grid-cols-3 md:gap-6 border-'>
-          <div className='px-6 sm:px-0'>
-            <h3 className='px-6 before:text-base font-semibold leading-6 text-gray-900'>
-              Healthcare Provider Information
-            </h3>
-          </div>
-          <div className=' md:col-span-1 '></div>
-          <div className='mt-5 md:col-span-2 md:mt-0'>
-            <form onSubmit={formik.handleSubmit} className='form'>
+        <h3 className='text-base p-4 mt-4 mb-2 text-center font-bold leading-6 text-gray-900'>
+          Health Provider Information
+        </h3>
+        <div className='md:grid md:grid-cols-0 md:gap-0'>
+          {/* <div className="md:col-span-1">
+                          <div className="px-4 sm:px-0">
+                              <p className="mt-1 text-sm text-gray-600">Use a permanent address where you can receive mail.</p>
+                          </div>
+                      </div> */}
+          <div className='mt-5 md:col-span-2 md:mt-0 border-2 border-violet-200'>
+            <form action='#' >
               <div className='overflow-hidden shadow sm:rounded-md'>
                 <div className='bg-white px-4 py-5 sm:p-6'>
                   <div className='grid grid-cols-6 gap-6'>
@@ -57,16 +38,11 @@ const RegisterHP = () => {
                         type='text'
                         name='hp_id'
                         id='hp_id'
-                        value={formik.values.hp_id}
-                        onChange={formik.handleChange}
-                        error={
-                          formik.touched.hp_id && Boolean(formik.errors.hp_id)
-                        }
-                        helperText={formik.touched.hp_id && formik.errors.hp_id}
                         autoComplete='given-name'
                         className='mt-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
                       />
                     </div>
+
                     <div className='col-span-6 sm:col-span-3'>
                       <label
                         htmlFor='last-name'
@@ -79,15 +55,11 @@ const RegisterHP = () => {
                         name='name'
                         id='name'
                         autoComplete='family-name'
-                        onChange={formik.handleChange}
-                        error={
-                          formik.touched.name && Boolean(formik.errors.name)
-                        }
-                        helperText={formik.touched.name && formik.errors.name}
                         className='mt-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
                       />
                     </div>
-                    <div className='col-span-6'>
+
+                    <div className='col-span-6 sm:col-span-4'>
                       <label
                         htmlFor='street-address'
                         className='block text-sm font-medium leading-6 text-gray-900'
@@ -98,20 +70,28 @@ const RegisterHP = () => {
                         type='text'
                         name='address'
                         id='address'
-                        onChange={formik.handleChange}
-                        error={
-                          formik.touched.address &&
-                          Boolean(formik.errors.address)
-                        }
-                        helperText={
-                          formik.touched.address && formik.errors.address
-                        }
                         autoComplete='street-address'
                         className='mt-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
                       />
                     </div>
 
-                    <div className='col-span-6 sm:col-span-6 lg:col-span-2'>
+                    {/* <div className="col-span-6 sm:col-span-3">
+                                              <label htmlFor="country" className="block text-sm font-medium leading-6 text-gray-900">
+                                                  Country
+                                              </label>
+                                              <select
+                                                  id="country"
+                                                  name="country"
+                                                  autoComplete="country-name"
+                                                  className="mt-2 px-2 block w-full rounded-md border-0 bg-white py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-1 focus:ring-indigo-100 sm:text-sm sm:leading-6"
+                                              >
+                                                  <option>United States</option>
+                                                  <option>Canada</option>
+                                                  <option>Mexico</option>
+                                              </select>
+                                          </div> */}
+
+                    <div className='col-span-6'>
                       <label
                         htmlFor='city'
                         className='block text-sm font-medium leading-6 text-gray-900'
@@ -122,18 +102,41 @@ const RegisterHP = () => {
                         type='text'
                         name='phoneNo'
                         id='phoneNo'
-                        onChange={formik.handleChange}
-                        error={
-                          formik.touched.phoneNo &&
-                          Boolean(formik.errors.phoneNo)
-                        }
-                        helperText={
-                          formik.touched.phoneNo && formik.errors.phoneNo
-                        }
                         autoComplete='address-level2'
                         className='mt-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
                       />
                     </div>
+
+                    
+
+                    
+
+                    
+
+                    
+
+                    {/* <div className="col-span-6 sm:col-span-6 lg:col-span-2">
+                                          <div className="flex items-center justify-center">
+                                              <div
+                                                  className="relative mb-3 xl:w-96"
+                                                  data-te-datepicker-init
+                                                  data-te-input-wrapper-init>
+                                                       <label
+                                                      for="floatingInput"
+                                                      className="pointer-events-none absolute top-0 left-3 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[2.15] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[1.15rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[1.15rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-neutral-200"
+                                                  >Select a date</label
+                                                  >
+                                                  <input
+                                                      type="text"
+                                                      className="peer block min-h-[auto] w-full rounded border-0 bg-transparent py-[0.32rem] px-3 leading-[2.15] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
+                                                      placeholder="Select a date" />
+                                                 
+                                                  </div>
+                                              </div>
+                                          </div> */}
+
+
+                    
                   </div>
                 </div>
                 <div className='bg-gray-50 px-4 py-3 text-right sm:px-6'>
@@ -151,6 +154,4 @@ const RegisterHP = () => {
       </div>
     </>
   );
-};
-
-export default RegisterHP;
+}
