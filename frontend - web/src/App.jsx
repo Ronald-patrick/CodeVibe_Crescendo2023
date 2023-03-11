0f3ac135e5cd001ae98844e4bf3ff62ba8b985fe
bhaven01-geek <65948166+bhaven01-geek@users.noreply.github.com>
2023-03-11 20:12:15 +0530

    work:Integrated Dashboard with Pages

--- frontend - web/src/App.jsx
+++ frontend - web/src/App.jsx
@@ -7,18 +7,20 @@ import PatientRegister from './pages/PatientRegister';
 import SearchPatientList from './pages/SearchPatientList';
 import SendRequest from './pages/SendRequest';
 import Dashboard from './pages/Dashboard';
+import PatientDetailsCard from './pages/PatientDetailsCard';
 
 function App() {
   return (
   <Routes>
-    <Route path='/' element={<Landing />} />
+    <Route path = "/Dashboard" element={<Dashboard/>} >
+    </Route>
+        <Route path='patientRegister' element={<PatientRegister />} />
+        <Route path = "listPatients" element={<SearchPatientList/>} />
+    <Route index path='/' element={<Landing />} />
     <Route path='/login' element={<Login />} />
     <Route path='/register' element={<Register />} />
-    <Route path='/patientRegister' element={<PatientRegister />} />
-    <Route path = "/listPatients" element={<SearchPatientList/>} />
     <Route path = "/sendReq" element={<SendRequest/>} />
-    <Route path = "/Dashboard" element={<Dashboard/>} />
-  
+    <Route path = "/patdet" element={<PatientDetailsCard/>} />
     
 
   </Routes>
--- frontend - web/src/pages/SearchPatientList.jsx
+++ frontend - web/src/pages/SearchPatientList.jsx
@@ -2,7 +2,7 @@
 const SearchPatientList = () => {
     return (
         <>
-            <div class="bg-white -lime-700 pt-12 pr-0 pb-12 pl-0 mt-0 mr-auto mb-0 ml-auto sm:py-16 lg:py-20">
+            <div class="bg-white  pt-0 pr-0 pb-0 pl-0 mt-0 mr-auto mb-0 ml-auto sm:py-16 lg:py-6">
                 <div class="pt-0 pr-4 pb-0 pl-4 mt-0 mr-auto mb-0 ml-auto max-w-7xl sm:px-6 lg:px-8">
                     <div class="pt-0 pr-4 pb-0 pl-4 mt-0 mr-auto mb-0 ml-auto max-w-4xl sm:px-6 lg:px-8">
                         <div class="pt-0 pr-4 pb-0 pl-4 mt-0 mr-auto mb-0 ml-auto sm:flex sm:items-center sm:justify-between">
@@ -26,7 +26,8 @@ const SearchPatientList = () => {
                                 </div>
                             </div>
                         </div>
-                        <div class="border-2 border-gray-300 shadow-xl mt-8 mr-0 mb-0 ml-0 pt-4 pr-10 pb-4 pl-10 flow-root rounded-lg sm:py-2">
+                    
+                        <div class="border-2 border-violet-300 shadow-xl mt-8 mr-0 mb-0 ml-0 pt-4 pr-10 pb-4 pl-10 flow-root rounded-lg sm:py-2">
                             <div class="pt--10 pr-0 pb-10 pl-0">
                                 <div class="pt-5 pr-0 pb-0 pl-0 mt-5 mr-0 mb-0 ml-0">
                                     <div class="sm:flex sm:items-center sm:justify-between sm:space-x-5">
