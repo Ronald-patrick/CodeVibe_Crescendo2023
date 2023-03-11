feb90301358ffa95bfd65243efb5906972b25c92
iammeet071 <meetparmar071@gmail.com>
2023-03-11 17:11:16 +0530

    changed reportlist

--- frontend - web/src/components/ReportList.jsx
+++ frontend - web/src/components/ReportList.jsx
@@ -14,115 +14,115 @@ function ReportList() {
           <div className='overflow-x-auto shadow-md sm:rounded-lg'>
             <div className='inline-block min-w-full align-middle'>
               <div className='overflow-hidden '>
-                <table className='min-w-full divide-y divide-gray-200 table-fixed dark:divide-gray-700'>
-                  <thead className='bg-gray-100 dark:bg-gray-700'>
+                <table className='min-w-full divide-y divide-primary table-fixed dark:divide-primary'>
+                  <thead className='bg-gray-100 dark:bg-primary'>
                     <tr>
                       <th scope='col' className='p-4'></th>
                       <th
                         scope='col'
-                        className='py-3 px-6 text-xs font-medium tracking-wider text-left text-gray-700 uppercase dark:text-gray-400'
+                        className='py-3 px-6 text-xs font-medium tracking-wider text-left text-white uppercase'
                       >
                         Patient Name
                       </th>
                      
                       <th
                         scope='col'
-                        className='py-3 px-6 text-xs font-medium tracking-wider text-left text-gray-700 uppercase dark:text-gray-400'
+                        className='py-3 px-6 text-xs font-medium tracking-wider text-left text-white uppercase '
                       >
                         Date
                       </th>
                       <th
                         scope='col'
-                        className='py-3 px-6 text-xs font-medium tracking-wider text-left text-gray-700 uppercase dark:text-gray-400'
+                        className='py-3 px-6 text-xs font-medium tracking-wider text-left text-white uppercase '
                       >
                         View
                       </th>
                     </tr>
                   </thead>
-                  <tbody className='bg-white divide-y divide-gray-200 dark:bg-gray-800 dark:divide-gray-700'>
-                    <tr className='hover:bg-gray-100 dark:hover:bg-gray-700'>
+                  <tbody className='bg-white divide-y divide-primary  dark:divide-primary'>
+                    <tr className='hover:bg-primary '>
                       <td className='p-4 w-4'></td>
-                      <td className='py-4 px-6 text-sm font-medium text-gray-900 whitespace-nowrap dark:text-white'>
+                      <td className='py-4 px-6 text-sm font-medium  whitespace-nowrap text-black'>
                         Meet
                       </td>
                       
-                      <td className='py-4 px-6 text-sm font-medium text-gray-900 whitespace-nowrap dark:text-white'>
+                      <td className='py-4 px-6 text-sm font-medium  whitespace-nowrap text-black'>
                        07-11-2022
                       </td>
                       <td className='py-4 px-6 text-sm font-medium  whitespace-nowrap'>
                         <a
                           href='#'
-                          className='text-blue-600  dark:text-blue-500 hover:underline'
+                          className='text-primary  dark:text-primary hover:underline'
                         >
                           View
                         </a>
                       </td>
                     </tr>
-                    <tr className='hover:bg-gray-100 dark:hover:bg-gray-700'>
+                    <tr className='hover:bg-primary '>
                       <td className='p-4 w-4'></td>
-                      <td className='py-4 px-6 text-sm font-medium text-gray-900 whitespace-nowrap dark:text-white'>
+                      <td className='py-4 px-6 text-sm font-medium  whitespace-nowrap text-black'>
                         Ronald
                       </td>
                       
-                      <td className='py-4 px-6 text-sm font-medium text-gray-900 whitespace-nowrap dark:text-white'>
+                      <td className='py-4 px-6 text-sm font-medium whitespace-nowrap text-black'>
                       21-01-2022
                       </td>
                       <td className='py-4 px-6 text-sm font-medium  whitespace-nowrap'>
                         <a
                           href='#'
-                          className='text-blue-600  dark:text-blue-500 hover:underline'
+                          className='text-primary  dark:text-primaryhover:underline'
                         >
                           View
                         </a>
                       </td>
                     </tr>
-                    <tr className='hover:bg-gray-100 dark:hover:bg-gray-700'>
+                    <tr className='hover:bg-primary '>
                       <td className='p-4 w-4'></td>
-                      <td className='py-4 px-6 text-sm font-medium text-gray-900 whitespace-nowrap dark:text-white'>
+                      <td className='py-4 px-6 text-sm font-medium whitespace-nowrap text-black'>
                         Raj
                       </td>
                       
-                      <td className='py-4 px-6 text-sm font-medium text-gray-900 whitespace-nowrap dark:text-white'>
+                      <td className='py-4 px-6 text-sm font-medium  whitespace-nowrap text-black'>
                       19-12-2022
                       </td>
                       <td className='py-4 px-6 text-sm font-medium  whitespace-nowrap'>
                         <a
                           href='#'
-                          className='text-blue-600  dark:text-blue-500 hover:underline'
+                          className='text-primary  dark:text-primaryhover:underline'
                         >
                           View
                         </a>
                       </td>
                     </tr>
-                    <tr className='hover:bg-gray-100 dark:hover:bg-gray-700'>
+                    <tr className='hover:bg-primary '>
                       <td className='p-4 w-4'>
                         <div className='flex items-center'></div>
                       </td>
-                      <td className='py-4 px-6 text-sm font-medium text-gray-900 whitespace-nowrap dark:text-white'>
+                      <td className='py-4 px-6 text-sm font-medium text-gray-900 whitespace-nowrap text-black'>
                         Bhavendra
                       </td>
                      
-                      <td className='py-4 px-6 text-sm font-medium text-gray-900 whitespace-nowrap dark:text-white'>
+                      <td className='py-4 px-6 text-sm font-medium text-gray-900 whitespace-nowrap text-black'>
                         21-01-2023
                       </td>
                       <td className='py-4 px-6 text-sm font-medium  whitespace-nowrap'>
                         <a
                           href='#'
-                          className='text-blue-600  dark:text-blue-500 hover:underline'
+                          className='text-primary  dark:text-primaryhover:underline'
                         >
                           View
                         </a>
                       </td>
                     </tr>
-                    <tr className='hover:bg-gray-100 dark:hover:bg-gray-700'>
+                    <tr className='hover:bg-primary '>
                       <td className='p-4 w-4'>
                         <div className='flex items-center'></div>
                       </td>
-                      <td className='py-4 px-6 text-sm font-medium text-gray-900 whitespace-nowrap dark:text-white'>
+                      <td className='py-4 px-6 text-sm font-medium text-gray-900 whitespace-nowrap text-black'>
                         Rohit
                       </td>
                       
-                      <td className='py-4 px-6 text-sm font-medium text-gray-900 whitespace-nowrap dark:text-white'>
+                      <td className='py-4 px-6 text-sm font-medium text-gray-900 whitespace-nowrap text-black'>
                       21-02-2023
                       </td>
                       <td className='py-4 px-6 text-sm font-medium  whitespace-nowrap'>
@@ -130,7 +130,7 @@ function ReportList() {
                           href='#'
                           onClick={openClick}
                           target="_blank"
-                          className='text-blue-600  dark:text-blue-500 hover:underline'
+                          className='text-primary  dark:text-primaryhover:underline'
                         >
                           View
                         </a>
