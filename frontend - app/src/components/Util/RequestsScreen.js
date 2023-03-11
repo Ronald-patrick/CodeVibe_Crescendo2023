6c8ebe600f18bc4c86c8beed2a54cc692d0233c2
Raj Mourya <rajhmourya@gmail.com>
2023-03-11 22:45:57 +0530

    dummy

--- frontend - app/src/components/Util/RequestsScreen.js
+++ frontend - app/src/components/Util/RequestsScreen.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import React,{useState,useEffect} from 'react';
 import {FlatList, StyleSheet, Text, View} from 'react-native';
 import RequestsItem from './RequestsItem';
-
+import axios from 'axios';
 const data = [
   {id: '1', PH1Name: 'Item 1',PHPhone: 'Phone 1',PH2Name: 'Item 2'},
   {id: '2', PH1Name: 'Item 1',PHPhone: 'Phone 1',PH2Name: 'Item 2'},
@@ -19,7 +20,45 @@ const renderItem = ({item}) => (
 </View>
 );
 
+
+_retrieveData = async () => {
+  try {
+    const value = await AsyncStorage.getItem('token');
+    if (value !== null) {
+      // We have data!!
+      console.log('inside func', value);
+      return value;
+    }
+  } catch (error) {
+    // Error retrieving data
+    console.log(error);
+  }
+};
+
 export default function RequestsScreen() {
+
+  const [data, setData] = useState([]);
+
+
+  const demo = async () => {
+    let tokenGot = await _retrieveData();
+
+    const config = {
+      headers: {Authorization: `Bearer ${tokenGot}`},
+    };
+
+    //   console.log(config);
+    const res = await axios.post(
+      'http://localhost:5000/api/patient/get-requests',
+      {},
+      config,
+    );
+    console.log("requests apge: ",res.data)
+    // setData(ls);
+  };
+  useEffect(() => {
+   demo();
+  }, []);
   return (
     <View style={styles.container}>
       <FlatList
