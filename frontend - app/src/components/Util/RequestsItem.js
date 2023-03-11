a41a668c7a284756573c1829fc7114eb6a826e6f
Raj Mourya <rajhmourya@gmail.com>
2023-03-11 23:51:10 +0530

    add actions on deny or allow

--- frontend - app/src/components/Util/RequestsItem.js
+++ frontend - app/src/components/Util/RequestsItem.js
@@ -1,19 +1,55 @@
 import React from 'react';
 import {View, StyleSheet, Text, TouchableOpacity} from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
-import { StripeProvider } from '@stripe/stripe-react-native';
+import {StripeProvider} from '@stripe/stripe-react-native';
+import axios from 'axios';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { useNavigation } from '@react-navigation/native';
 
-const RequestsItem = ({item}) => {
 
-  const handleIconClick = (type)=>{
-    console(type);
+_retrieveData = async () => {
+  try {
+    const value = await AsyncStorage.getItem('token');
+    if (value !== null) {
+      // We have data!!
+      console.log('inside func', value);
+      return value;
+    }
+  } catch (error) {
+    // Error retrieving data
+    console.log(error);
   }
+};
+
+const RequestsItem = ({item,refreshData}) => {
+
+  let navigation  =  useNavigation();
+
+  const handleIconClick = async type => {
+    let tokenGot = await _retrieveData();
+
+    const config = {
+      headers: {Authorization: `Bearer ${tokenGot}`},
+    };
+
+    //   console.log(config);
+    const res = await axios.post(
+      'http://localhost:5000/api/patient/verify-request',
+      {isVerified: type === 'allow' ? true : false, id: item.id},
+      config,
+    );
+
+    // navigation.navigate('Home');
+
+    console.log({isVerified: type === 'allow' ? true : false, id: item.id});
+    refreshData();
+  };
 
   return (
     <View style={styles.container}>
       <View style={styles.card}>
         {/* your card content goes here */}
-        <Text style={{color:'#2735AD'}}>Has Requested to access your EHR</Text>
+        <Text style={{color: '#2735AD'}}>Has Requested to access your EHR</Text>
         <Text>{item.name}</Text>
         <Text>{item.address}</Text>
         <Text>{item.phone_number}</Text>
@@ -21,14 +57,20 @@ const RequestsItem = ({item}) => {
 
       <View style={styles.buttonsContainer}>
         <TouchableOpacity style={styles.button}>
-          <Icon name="close" size={24} color="white" style={styles.denyButton} onPress={()=>handleIconClick('deny')}/>
+          <Icon
+            name="close"
+            size={24}
+            color="white"
+            style={styles.denyButton}
+            onPress={() => handleIconClick('deny')}
+          />
         </TouchableOpacity>
         <TouchableOpacity style={styles.button}>
           <Icon
             name="check"
             size={24}
             color="white"
-            onPress={()=>handleIconClick('allow')}
+            onPress={() => handleIconClick('allow')}
             style={styles.allowButton}
           />
         </TouchableOpacity>
@@ -57,15 +99,14 @@ const styles = StyleSheet.create({
     shadowOpacity: 0.25,
     shadowRadius: 4,
     elevation: 5,
-    marginBottom:5
+    marginBottom: 5,
   },
   buttonsContainer: {
     flex: 1,
     flexDirection: 'row',
     width: '80%',
-    alignContent:'center',
+    alignContent: 'center',
     justifyContent: 'center',
-    
   },
   button: {
     backgroundColor: '#fff',
@@ -77,7 +118,6 @@ const styles = StyleSheet.create({
       width: 1,
       height: 10,
     },
-   
   },
   buttonText: {
     fontSize: 16,
@@ -91,12 +131,11 @@ const styles = StyleSheet.create({
     color: 'green',
   },
   allowIcon: {
-    backgroundColor: 'green'
-  }
-  ,
+    backgroundColor: 'green',
+  },
   denyIcon: {
-    backgroundColor: 'red'
-  }
+    backgroundColor: 'red',
+  },
 });
 
 export default RequestsItem;
